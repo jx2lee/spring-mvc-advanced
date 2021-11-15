83109ea468ac1dc5b460308f830b49c70ab63314
jx2lee <jaejun.lee.1991@gmail.com>
2021-11-16 00:32:03 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술: 타임리 - 스프링 통합과 폼: 입력 폼 처리

---
+++ thymeleaf-with-spring/src/main/java/com/jx2lee/thymeleafwithspring/ThymeleafWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jx2lee.thymeleafwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafWithSpringApplication.class, args);
+    }
+
+}
---
+++ thymeleaf-with-spring/src/test/java/com/jx2lee/thymeleafwithspring/ThymeleafWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jx2lee.thymeleafwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
