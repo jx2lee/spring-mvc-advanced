82d6a88827ad15f25750bc68fcd64c3da0789ac0
jx2lee <jaejun.lee.1991@gmail.com>
2021-12-02 23:37:39 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술: Validation: FieldError, ObjectError
    * FieldError 는 2개 생성자를 제공하는데 이때 rejectValue 를 설정하면 기존값이 보존된다.

--- spring-mvc/src/main/java/io/github/jx2lee/completed/web/form/validation/ValidationItemControllerV2.java
+++ spring-mvc/src/main/java/io/github/jx2lee/completed/web/form/validation/ValidationItemControllerV2.java
@@ -68,8 +68,8 @@ public class ValidationItemControllerV2 {
         return "validation/form/v2/addForm";
     }
 
-    @PostMapping("/add")
-    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+    // @PostMapping("/add")
+    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         // 특정Field error 처리
         if (!StringUtils.hasText(item.getItemName())) {
@@ -91,6 +91,43 @@ public class ValidationItemControllerV2 {
             }
         }
 
+        log.info("errors={}", bindingResult);
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            return "validation/form/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/form/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        // 특정Field error 처리
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null,  "가격은 1,000원 - 1,000,000 까지 허용합니다"));
+
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999 까지 허용합니다"));
+        }
+
+        // 복합Field error 처리
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+
+        log.info("errors={}", bindingResult);
         //검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
             return "validation/form/v2/addForm";
