48d3f128f9cd0fc0f7e85e7d02d94fb979d40f5f
jx2lee <jaejun.lee.1991@gmail.com>
2021-12-03 23:09:47 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술: Validation: 오류 코드와 메시지 처리1
    * errors.properties 로 에러 메세지를 지정하여 사용할 수 있다.
    * 하지만 뭔가 좀 너저분하고 귀찮은데..

--- spring-mvc/src/main/java/io/github/jx2lee/completed/web/form/validation/ValidationItemControllerV2.java
+++ spring-mvc/src/main/java/io/github/jx2lee/completed/web/form/validation/ValidationItemControllerV2.java
@@ -104,7 +104,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/form/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+    // @PostMapping("/add")
     public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         // 특정Field error 처리
@@ -124,6 +124,42 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+
+        log.info("errors={}", bindingResult);
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            return "validation/form/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/form/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        // 특정Field error 처리
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, null));
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 10000000},  null));
+
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"totalPriceMin"}, new Object[]{9999}, null));
+        }
+
+        // 복합Field error 처리
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice}, null));
             }
         }
 
