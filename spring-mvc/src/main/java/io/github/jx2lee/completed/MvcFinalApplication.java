f2de96ab1d99978c6ad13a8a208bc358833ee657
jx2lee <jaejun.lee.1991@gmail.com>
2021-12-12 18:24:04 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술: 로그인 처리1 - 쿠키, 세션: 로그인 처리하기 - 쿠키 사용

--- spring-mvc/src/main/java/io/github/jx2lee/completed/MvcFinalApplication.java
+++ spring-mvc/src/main/java/io/github/jx2lee/completed/MvcFinalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalApplication {
+public class MvcFinalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FinalApplication.class, args);
+        SpringApplication.run(MvcFinalApplication.class, args);
     }
 
     // @Bean
--- spring-mvc/src/main/java/io/github/jx2lee/completed/web/login/LoginController.java
+++ spring-mvc/src/main/java/io/github/jx2lee/completed/web/login/LoginController.java
@@ -2,14 +2,20 @@ package io.github.jx2lee.completed.web.login;
 
 import io.github.jx2lee.completed.domain.login.LoginService;
 import io.github.jx2lee.completed.domain.member.Member;
+import io.github.jx2lee.completed.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Controller
@@ -17,10 +23,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class LoginController {
 
     private final LoginService loginService;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/login")
-    public String home() {
-        return "login/home";
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if (memberId == null) {
+            return "/login/home";
+        }
+
+        // 로그인
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "/login/home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "/login/loginHome";
     }
 
     @GetMapping("/login/loginForm")
@@ -29,7 +47,9 @@ public class LoginController {
     }
 
     @PostMapping("/login/loginForm")
-    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Validated @ModelAttribute LoginForm form,
+                        BindingResult bindingResult,
+                        HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "/login/loginForm";
         }
@@ -43,7 +63,21 @@ public class LoginController {
         }
 
         // 로그인 성공 처리
-
+        // Cookie 생성
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
         return "redirect:/login";
+    }
+
+    @PostMapping("/login/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/login";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
