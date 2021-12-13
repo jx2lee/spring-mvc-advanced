987ea4d9d85f0d090df8228cf35f75ff224e903d
jx2lee <jaejun.lee.1991@gmail.com>
2021-12-13 12:19:48 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술: 로그인 처리1 - 쿠키, 세션: 로그인 처리하기 - 서블릿 HTTP 세션1
    * HttpSession
    * RequestServlet 으로 부터 HttpSession 객체를 생성한다.
    * JSESSIONID 생성 확인

---
+++ spring-mvc/src/main/java/io/github/jx2lee/completed/web/login/LoginConst.java
@@ -0,0 +1,5 @@
+package io.github.jx2lee.completed.web.login;
+
+public interface LoginConst {
+    String LOGIN_MEMBER = "loginMember";
+}
--- spring-mvc/src/main/java/io/github/jx2lee/completed/web/login/LoginController.java
+++ spring-mvc/src/main/java/io/github/jx2lee/completed/web/login/LoginController.java
@@ -3,6 +3,7 @@ package io.github.jx2lee.completed.web.login;
 import io.github.jx2lee.completed.domain.login.LoginService;
 import io.github.jx2lee.completed.domain.member.Member;
 import io.github.jx2lee.completed.domain.member.MemberRepository;
+import io.github.jx2lee.completed.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -15,7 +16,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -24,9 +27,10 @@ public class LoginController {
 
     private final LoginService loginService;
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
-    @GetMapping("/login")
-    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+    // @GetMapping("/login")
+    public String homeLoginV1(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
         if (memberId == null) {
             return "/login/home";
         }
@@ -41,15 +45,49 @@ public class LoginController {
         return "/login/loginHome";
     }
 
+    // @GetMapping("/login")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+        // 세션 관리자에 저장된 회원정보를 조회한다.
+        Member loginMember = (Member) sessionManager.getSession(request);
+
+        // 로그인
+        if (loginMember == null) {
+            return "/login/home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "/login/loginHome";
+    }
+
+    @GetMapping("/login")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+
+        // 세션이 없다면 로그인 홈 화면을 노출한다.
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "/login/home";
+        }
+
+        // 로그인
+        Member loginMember = (Member) session.getAttribute(LoginConst.LOGIN_MEMBER);
+        if (loginMember == null) {
+            return "/login/home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "/login/loginHome";
+    }
+
     @GetMapping("/login/loginForm")
     public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
         return "/login/loginForm";
     }
 
-    @PostMapping("/login/loginForm")
-    public String login(@Validated @ModelAttribute LoginForm form,
+    // @PostMapping("/login/loginForm")
+    public String loginV1(@Validated @ModelAttribute LoginForm form,
                         BindingResult bindingResult,
                         HttpServletResponse response) {
+
         if (bindingResult.hasErrors()) {
             return "/login/loginForm";
         }
@@ -69,9 +107,74 @@ public class LoginController {
         return "redirect:/login";
     }
 
-    @PostMapping("/login/logout")
-    public String logout(HttpServletResponse response) {
+    // @PostMapping("/login/loginForm")
+    public String loginV2(@Validated @ModelAttribute LoginForm form,
+                        BindingResult bindingResult,
+                        HttpServletResponse response) {
+
+        if (bindingResult.hasErrors()) {
+            return "/login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        log.info("login? {}", loginMember);
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공 처리
+        // 세션 관리자를 통해 세션을 생성하고 회원 데이터를 보관한다.
+        sessionManager.createSession(loginMember, response);
+
+        return "redirect:/login";
+    }
+
+    @PostMapping("/login/loginForm")
+    public String loginV3(@Validated @ModelAttribute LoginForm form,
+                        BindingResult bindingResult,
+                        HttpServletRequest request) {
+
+        if (bindingResult.hasErrors()) {
+            return "/login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        log.info("login? {}", loginMember);
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공 처리
+        // 세션이 있으면 세션을 반환하고 없다면 신규 세션 생성
+        HttpSession session = request.getSession();
+        // 세션에 로그인 회원 정보를 보관한다.
+        session.setAttribute(LoginConst.LOGIN_MEMBER, loginMember);
+
+        return "redirect:/login";
+    }
+
+    // @PostMapping("/login/logout")
+    public String logoutV1(HttpServletResponse response) {
         expireCookie(response, "memberId");
+        return "redirect:/login";
+    }
+
+    // @PostMapping("/login/logout")
+    public String logoutV2(HttpServletRequest request) {
+        sessionManager.expire(request);
+        return "redirect:/login";
+    }
+
+    @PostMapping("/login/logout")
+    public String logoutV3(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
         return "redirect:/login";
     }
 
