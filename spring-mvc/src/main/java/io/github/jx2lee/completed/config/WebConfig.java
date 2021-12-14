83a037dcc1d63f0d0df786b3dfd202c7a2d1e70b
jx2lee <jaejun.lee.1991@gmail.com>
2021-12-14 14:16:02 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술: 로그인 처리2 - 필터, 인터셉터: 서블릿 필터 - 요청 로그
    * Filter 를 상속받은 구현체 LogFilter 생성
    * doFilter 가 가장 중요
    * config 적용 후 확인하면 log 를 확인할 수 있다.
    * 참고: logback mdc

---
+++ spring-mvc/src/main/java/io/github/jx2lee/completed/config/WebConfig.java
@@ -0,0 +1,21 @@
+package io.github.jx2lee.completed.config;
+
+import io.github.jx2lee.completed.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+        return filterFilterRegistrationBean;
+    }
+}
