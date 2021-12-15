b88cc981de9a3f0db80e0aae4a10be855a701295
jx2lee <jaejun.lee.1991@gmail.com>
2021-12-15 21:17:40 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술: 로그인 처리2 - 필터, 인터셉터: 스프링 인터셉터 - 요청로그
    * Spring 에서 제공하는 Interceptor 로 요청로그 대체
    * WebConfig 에 WebMvcConfigurer 를 상속받아 addInterceptors 를 overriding 한다.
    * 빌더 패턴으로 가시적으로 조건 설정이 가능한 것 같다.
    * parttern document: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/util/pattern/PathPattern.html

--- spring-mvc/src/main/java/io/github/jx2lee/completed/config/WebConfig.java
+++ spring-mvc/src/main/java/io/github/jx2lee/completed/config/WebConfig.java
@@ -2,16 +2,29 @@ package io.github.jx2lee.completed.config;
 
 import io.github.jx2lee.completed.web.filter.LogFilter;
 import io.github.jx2lee.completed.web.filter.LoginCheckFilter;
+import io.github.jx2lee.completed.web.interceptor.LogInterceptor;
+import lombok.extern.java.Log;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+    }
+
+    // @Bean
+    // 인터셉터 적용을 위해 잠시 주서거리
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
