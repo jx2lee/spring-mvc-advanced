592cf2865f9a40811b32f1f2b3cacd308e2879e7
jx2lee <jaejun.lee.1991@gmail.com>
2021-12-18 11:23:18 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술: 예외 처리와 오류 페이지: 서블릿 예외 처리 - 인터셉터

--- spring-mvc-exception/src/main/java/io/github/jx2lee/springmvcexception/exception/config/WebConfig.java
+++ spring-mvc-exception/src/main/java/io/github/jx2lee/springmvcexception/exception/config/WebConfig.java
@@ -1,9 +1,11 @@
 package io.github.jx2lee.springmvcexception.exception.config;
 
 import io.github.jx2lee.springmvcexception.exception.filter.LogFilter;
+import io.github.jx2lee.springmvcexception.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -12,7 +14,15 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**");
+    }
+
+    // @Bean
     public FilterRegistrationBean<Filter> logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setOrder(1);
