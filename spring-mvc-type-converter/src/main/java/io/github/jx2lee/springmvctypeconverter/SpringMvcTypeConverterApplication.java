9c493e39687d553370803a65ce907cbc581e6192
jx2lee <jaejun.lee.1991@gmail.com>
2021-12-20 11:30:40 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술: 스프링 타입 컨버터: 스프링 타입 컨버터 소개
    * HTTP 요청 파라미터는 모두 문자로 처리
    * 스프링은 확장 가능한 컨버터 인터페이스를 제공

---
+++ spring-mvc-type-converter/src/main/java/io/github/jx2lee/springmvctypeconverter/SpringMvcTypeConverterApplication.java
@@ -0,0 +1,13 @@
+package io.github.jx2lee.springmvctypeconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcTypeConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcTypeConverterApplication.class, args);
+    }
+
+}
---
+++ spring-mvc-type-converter/src/test/java/io/github/jx2lee/springmvctypeconverter/SprigMvcTypeConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jx2lee.springmvctypeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigMvcTypeConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
