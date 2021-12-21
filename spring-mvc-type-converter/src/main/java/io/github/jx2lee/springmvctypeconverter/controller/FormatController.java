3cb0cc409645d2dd1bde96c9aa1207560f93e4d1
jx2lee <jaejun.lee.1991@gmail.com>
2021-12-21 22:37:01 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술: 스프링 타입 컨버터: 스프링이 제공하는 기본 Formatter
    * 애노테이션 기반으로 원하는 형식을 지정해서 사용할 수 있는 매우 유용한 포맷터 두 가지를 기본으로 제공
    * NumberFormat, DateTimeFormat
    * https://docs.spring.io/spring-framework/docs/current/reference/html/core.html#format-CustomFormatAnnotations

---
+++ spring-mvc-type-converter/src/main/java/io/github/jx2lee/springmvctypeconverter/controller/FormatController.java
@@ -0,0 +1,40 @@
+package io.github.jx2lee.springmvctypeconverter.controller;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class FormatController {
+
+    @GetMapping("/formatter/edit")
+    public String formatterHome(Model model) {
+        Form form = new Form();
+        form.setNumber(100000);
+        form.setLocalDateTime(LocalDateTime.now());
+
+        model.addAttribute("form", form);
+        return "/formatter-form";
+    }
+
+    @PostMapping("/formatter/edit")
+    public String formatterEdit(@ModelAttribute Form form) {
+        return "/formatter-view";
+    }
+
+    @Data
+    static class Form {
+
+        @NumberFormat(pattern = "###,###")
+        private Integer number;
+        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime localDateTime;
+    }
+}
