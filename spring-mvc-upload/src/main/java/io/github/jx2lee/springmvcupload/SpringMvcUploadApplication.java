d9fcfaf951a126fcf87cbd0e7abf80e3c9d278ce
jx2lee <jaejun.lee.1991@gmail.com>
2021-12-22 21:10:57 +0900

    init: 파일 업로드

---
+++ spring-mvc-upload/src/main/java/io/github/jx2lee/springmvcupload/SpringMvcUploadApplication.java
@@ -0,0 +1,13 @@
+package io.github.jx2lee.springmvcupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcUploadApplication.class, args);
+    }
+
+}
---
+++ spring-mvc-upload/src/test/java/io/github/jx2lee/springmvcupload/SpringMvcUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jx2lee.springmvcupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
